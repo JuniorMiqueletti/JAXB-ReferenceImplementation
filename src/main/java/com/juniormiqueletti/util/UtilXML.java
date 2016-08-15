34cb58c098e41c4348439462c69503a34bb3050a
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-08-15 09:51:49 -0300

    Creating UtilXML to easely parse

--- src/main/java/com/juniormiqueletti/util/UtilXML.java
+++ src/main/java/com/juniormiqueletti/util/UtilXML.java
@@ -1,5 +1,63 @@
 package com.juniormiqueletti.util;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
 public final class UtilXML {
+
+	public static Object parseObject(Object clazzToParse, File xmlFile) {
+
+		Object clazz = null;
+		JAXBContext jaxbContext;
+		Unmarshaller jaxbUnmarshaller;
+
+		try {
+
+			jaxbContext = createJaxbContext(clazzToParse);
+			
+			jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+			clazz = (Object) jaxbUnmarshaller.unmarshal(xmlFile);
+
+		} catch (JAXBException e) {
+
+			new RuntimeException(e);
+			
+		}
+
+		return clazz;
+
+	}
+
+	private static JAXBContext createJaxbContext(Object clazzToParse) throws JAXBException {
+		return JAXBContext.newInstance(clazzToParse.getClass());
+	}
+	
+	public static void parseToXmlFile(Object clazzToXml, File xmlFileToCreate){
+		
+		JAXBContext jaxbContext;
+	
+		Marshaller jaxbMarshaller;
+		
+		try {
+			
+			jaxbContext = createJaxbContext(clazzToXml);
+		
+			jaxbMarshaller = jaxbContext.createMarshaller();
+			
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			jaxbMarshaller.marshal(clazzToXml, xmlFileToCreate);
+
+		} catch (JAXBException e) {
+
+			new RuntimeException(e);
+		}
+		
+	}
 
 }
