990e3732df54d95716c7f286338d39f7a6f419d0
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-08-31 21:18:19 -0300

    Adding equals and hashcode methods

--- src/main/java/com/juniormiqueletti/domain/People.java
+++ src/main/java/com/juniormiqueletti/domain/People.java
@@ -1,4 +1,5 @@
 package com.juniormiqueletti.domain;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -7,53 +8,87 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class People {
 
 	private int id;
-    private String name;
-    private String nickName;
-    private int age;
+	private String name;
+	private String nickName;
+	private int age;
 
-    public String getNickName() {
-        return nickName;
-    }
+	public String getNickName() {
+		return nickName;
+	}
 
-    @XmlElement(name = "NICK_NAME")
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
+	@XmlElement(name = "NICK_NAME")
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @XmlElement(name = "NAME")
-    public void setName(String name) {
-        this.name = name;
-    }
+	@XmlElement(name = "NAME")
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    @XmlElement(name = "AGE")
-    public void setAge(int age) {
-        this.age = age;
-    }
+	@XmlElement(name = "AGE")
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    @XmlAttribute(name = "ID")
-    public void setId(int id) {
-        this.id = id;
-    }
+	@XmlAttribute(name = "ID")
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    @Override
-    public String toString() {
-        return "People{" +
-                "name='" + name + '\'' +
-                ", nickName='" + nickName + '\'' +
-                ", age=" + age +
-                ", id=" + id +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "People{" + "name='" + name + '\'' + ", nickName='" + nickName + '\'' + ", age=" + age + ", id=" + id
+				+ '}';
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((nickName == null) ? 0 : nickName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		People other = (People) obj;
+		if (age != other.age)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (nickName == null) {
+			if (other.nickName != null)
+				return false;
+		} else if (!nickName.equals(other.nickName))
+			return false;
+		return true;
+	}
+
 }
